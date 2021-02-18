c8141a9639fb11f45cd200abe6801b93e718d265
liuba <atamanyukl10@gmail.com>
2021-02-18 23:29:16 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lessonfive';
+  title = 'this is lesson five';
+
+  // TitleState(xxx): void {
+  //   console.log(xxx);
+  // }
+  user = {name: 'kokos', age: 45};
+users = [{name: 'kokos', age: 45}];
+  // checkForm(myForm: NgForm): void {
+  //   console.log(myForm);
+  // }
+  // @ts-ignore
+  name = new FormControl('kokos', [Validators.required, Validators.minLength(10)]);
+  age = new FormControl(123, Validators.required);
+
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+
+  checkForm(myForm): void{
+    this.users.push({name: myForm.controls.name.value, age: myForm.controls.age.value });
+    console.log(myForm);
+    console.log(this.users);
+  }
 }
+
